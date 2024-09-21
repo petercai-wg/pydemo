bc4bbe0cf6f5b2b0fcdbd634261f3ff675d1e0d7
petercai-wg <petercwg630@gmail.com>
2024-09-21 12:23:40 -0400

    fuzzy

--- core/strfuzzy.py
+++ core/strfuzzy.py
@@ -7,14 +7,29 @@ print(fuzz.ratio("Manual Charges", "manual charge"))
 # print(fuzz.ratio(" test", "This is a  test.", processor=None, score_cutoff= 85) )
 
 
+result = {}
 tab1 = ["Derivative","Sept Charge", "Additional Funding Cost", "Additional CFLA Cost", "1yr Liquidity Clearing - Loans", "Manual Charges", "Goodwill & Intagibles"]
 
 tab2 = ["Additional Funding Costs", "Loan", "manual charge", "GoodWill/Intangible", "September charges", "IHC", "Derivatives", "CFLA cost"]
 
 for t in tab1:
-    match_one = process.extractOne(t, tab2, scorer= fuzz.QRatio, score_cutoff= 65 )
+    match_one = process.extractOne(t, tab2, scorer= fuzz.QRatio, score_cutoff= 80 )
     print(f"t: {t},  {match_one}")
+    if match_one is not None:
+        v = match_one[0]
+        result[t] = v
+        tab1.remove(t)
+        tab2.remove(v)
 
+print(f"----- {tab1} ---------- ")
+print(f"----- {tab2} ---------- ")
 for t in tab1:
-    match_list = process.extractOne(t, tab2, scorer= fuzz.partial_token_ratio, score_cutoff= 65 )
-    print(f"t: {t},  {match_list}")
+    match_one = process.extractOne(t, tab2, scorer= fuzz.partial_token_ratio, score_cutoff= 60  )
+    print(f"t: {t},  {match_one}")
+    if match_one is not None:
+        v = match_one[0]
+        result[t] = v
+        tab1.remove(t)
+        tab2.remove(v)
+
+print(result)
